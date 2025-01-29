30479f9d08dbfce1f48996f0ae411f472a935bf8
Азат Мирсияпов <azatt2013@mail.ru>
2025-01-29 19:11:04 +0300

    Add files via upload

--- Проект 2/drawing_app.py
+++ Проект 2/drawing_app.py
@@ -23,6 +23,7 @@ class DrawingApp:
 
         self.canvas.bind('<B1-Motion>', self.paint)
         self.canvas.bind('<ButtonRelease-1>', self.reset)
+        self.canvas.bind('<Button-3>', self.pick_color)
 
 
     def setup_ui(self):
@@ -99,6 +100,14 @@ class DrawingApp:
                 file_path += '.png'
             self.image.save(file_path)
             messagebox.showinfo("Информация", "Изображение успешно сохранено!")
+
+    def pick_color(self, event):
+        """Метод выбора цвета пипеткой"""
+        x, y = event.x, event.y
+        pixel_color = self.image.getpixel((x, y))[:3]  # берем RGB компоненты без альфа-канала
+        hex_color = "#{:02X}{:02X}{:02X}".format(*pixel_color)
+        self.pen_color = hex_color
+        print(f"Выбран цвет: {hex_color}")
 
 
 def main():
--- Проект 2/readme.md
+++ Проект 2/readme.md
@@ -1,2 +1,3 @@
 ![alt text](screen/image1.png)
 ![alt text](screen/image2.png)
+![alt text](screen/image3.png)
